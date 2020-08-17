222f7c2772f149c337b7dc00820731a18621ef2d
stephaneparisot <stephane@wiplaymusic.com>
2020-08-18 00:05:40 +0200

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by sparisot on 18/08/2020.
+//  Copyright © 2020 Stéphane Parisot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
